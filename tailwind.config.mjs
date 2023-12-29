4c25e11f184ffa41e4a180eab1599662d3891010
Alessandro Rodríguez <alessrodrgz@gmail.com>
2023-12-29 22:50:59 +0100

    font sizes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontSize: {
+        heading: '2rem',
+        body: '1rem',
+        label: '0.875rem',
+        small: '0.625rem',
+        price: '0.75rem'
+      }
+    }
+  },
+  plugins: []
 }
