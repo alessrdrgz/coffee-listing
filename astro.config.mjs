e59e48c063178c76ade3af7edf27a2a6f7f3ad14
Alessandro Rodríguez <alessrodrgz@gmail.com>
2023-12-30 02:39:00 +0100

    updated site domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import tailwind from '@astrojs/tailwind';
-import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
+import preact from '@astrojs/preact'
+import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
-  site: 'http://127.0.0.1',
+  site: 'https://coffee.alessrodrgz.dev',
   output: 'server',
   adapter: vercel()
-});
+})
