d413a4b27b25ec3e4b2b33a04ba84cee6951b103
Alessandro Rodríguez <alessrodrgz@gmail.com>
2023-12-30 02:03:38 +0100

    Coffee endpoint

--- src/pages/coffee.ts
+++ src/pages/coffee.ts
@@ -1,6 +1,28 @@
+import Coffees from '@/data/coffees.json'
+import type { Coffee } from '@/types/Coffee'
 import type { APIRoute } from 'astro'
-import Coffees from '../data/coffees.json'
 
-export const GET: APIRoute = async (): Promise<Response> => {
-  return new Response(JSON.stringify(Coffees))
+export const GET: APIRoute = async ({ request }): Promise<Response> => {
+  const url = new URL(request.url)
+  const query = new URLSearchParams(url.search)
+
+  let data = [...Coffees]
+
+  for (const [key, value] of query.entries()) {
+    console.log({ key, value })
+    let parsedValue: boolean | string | number
+
+    switch (key) {
+      case 'available':
+        parsedValue = Boolean(value)
+        break
+
+      default:
+        parsedValue = value
+    }
+
+    data = data.filter((coffee) => coffee[key as keyof Coffee] === parsedValue)
+  }
+
+  return new Response(JSON.stringify(data))
 }
