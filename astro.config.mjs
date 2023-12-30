0a64ec0ca4aa3546bc681381a05a9bcac7146308
Alessandro Rodríguez <alessrodrgz@gmail.com>
2023-12-30 02:31:43 +0100

    vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-import tailwind from '@astrojs/tailwind'
-import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
 
-import preact from '@astrojs/preact'
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
   site: 'http://127.0.0.1',
-  output: 'server'
-})
+  output: 'server',
+  adapter: vercel()
+});
